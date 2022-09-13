7d3f1715d1f4a4ef9c7207b7e9b1620613c95fd7
artemyr <artem.r@affetta.ru>
2022-09-13 13:53:06 +0300

    lang

--- local/modules/affettaseo/lang/ru/options.php
+++ local/modules/affettaseo/lang/ru/options.php
@@ -2,4 +2,5 @@
 $MESS ['FORM_SAVE_CUSTOM'] = "Засейвить";
 $MESS ['MAIN_RESTORE_DEFAULTS'] = "По умолчанию";
 $MESS ['CLEAR_IMAGE_FOLDER'] = "Очистить кэш модуля";
+$MESS ['FORM_RESET'] = "Сбросить";
 ?>
